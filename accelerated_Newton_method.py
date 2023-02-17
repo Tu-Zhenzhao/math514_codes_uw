c21c2f33aa045a0adbc6bbd8d5f9deb7bc6727d2
tzz-MBA <tuzhenzhao@aol.com>
2023-02-16 23:24:51 -0600

    accelerated update new condition

--- accelerated_Newton_method.py
+++ accelerated_Newton_method.py
@@ -6,16 +6,30 @@ import matplotlib.pyplot as plt
 
 # Function to consider
 def f(x):
-    return e**x-x-4
+    #return e**x-x-4
+    #return x**2*(x-2)*(x-5)
+    if x>=0:
+        return x**(1/3.)
+    else:
+        return -(-x)**(1/3.)
 
 # Derivative of the function
 def df(x):
-    return e**x-1
+    #return e**x-1
+    #return 4*x**3-21*x**2+20*x
+    if x>=0:
+        return 1/(3*x**(2/3))
+    else:
+        return -1/(3*(-x)**(2/3))
 
 # double derivative of the funtion
 def ddf(x):
-    return  e**x
-
+    #return  e**x
+    #return 12*x**2-42*x+20
+    if x>=0:
+        return -2/(9*x**(5/3))
+    else:
+        return -2/(9*(-x)**(5/3))
 
 # determinter how many roots f has
 def root_test(x):
@@ -29,7 +43,7 @@ def delta(x):
     return df(x)**2-2*f(x)*ddf(x)
 
 # Starting guess
-x=1.5
+x=-1
 
 # exactly root
 x_ect = 6.093651116281923e-16
@@ -39,19 +53,20 @@ x_list = []
 
 # Perform ten steps of the accelerated Newton iteration
 count=0
-while abs(f(x)) > 1e-14:
+n=20
+#while abs(f(x)) > 1e-14:
+for i in range(n):
     # counting iterations
     count+=1
 
     #add x to list
-    #x_list.append(log(x))
+    #x_list.append(log(abs(x)))
+    x_list.append(x)
     # Take Newton step
     r_t = root_test(x)
     fv = f(x)
-    if r_t-fv == 0:
-        x-=df(x)/ddf(x)
-        print(count,x,fv)
-    elif r_t-fv > 0:
+
+    if delta(x) > 0:
         x_left = (ddf(x)*x-df(x)-sqrt(delta(x)))/ddf(x)
         x_right =  (ddf(x)*x-df(x)+sqrt(delta(x)))/ddf(x)
         if abs(x-x_left) < abs(x-x_right):
@@ -60,11 +75,18 @@ while abs(f(x)) > 1e-14:
         else:
             x = x_right
             print(count,x,fv)
+
     else:
-        print('second-order might be 0!')
-        break
+        x-=df(x)/ddf(x)
+        #print(count,x,fv)
+        print(ddf(x))
 
+    #else:
+    #    print('second-order might be 0!')
+    #    break
 
+plt.scatter([range(n)], x_list)
+plt.show()
 
 
 
